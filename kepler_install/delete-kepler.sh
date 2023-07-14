fc00ca7b923410f76f64c1627c26f1743277e49d
Adrian Hammond <adrian.hammond@redhat.com>
2023-07-14 12:40:24 +0100

    multiple updates

---
+++ kepler_install/delete-kepler.sh
@@ -0,0 +1,3 @@
+oc delete -f https://gist.githubusercontent.com/rootfs/047e06d3a8d31de6195c64143c900aee/raw/631c3bf77308bb99ebf89cf15385368e26e81a74/kepler-ocp.yaml
+
+oc delete -f kepler-ocp-4-13-airgap.yaml
